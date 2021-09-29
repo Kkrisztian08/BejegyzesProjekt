0027fb76a2a675d19c36ce512cf2d1b0eb157dc2
Kovacs Krisztian <k.krisztian0830@gmail.com>
2021-09-29 15:17:50 +0200

    b) feladat: új osztály létrehozása Bejegyzés néven             elkészités UML diagram alapján

---
+++ src/hu/petrik/Bejegyzes.java
@@ -0,0 +1,41 @@
+package hu.petrik;
+
+import java.time.LocalDateTime;
+
+public class Bejegyzes {
+    private String szerzo;
+    private String tartalom;
+    private int likeok=0;
+    private LocalDateTime letrejott=LocalDateTime.now();
+    private LocalDateTime szerkeztve=LocalDateTime.now();
+
+    public Bejegyzes(String szerzo, String tartalom){
+        this.szerzo=szerzo;
+        this.tartalom=tartalom;
+    }
+    public String getSzerzo(){
+        return this.szerzo;
+    }
+    public String getTartalom(){
+        return this.tartalom;
+    }
+    public void setTartalom(String tartalom){
+        this.tartalom=tartalom;
+        this.szerkeztve=LocalDateTime.now();
+    }
+    public int getLikeok(){
+        return this.likeok;
+    }
+    public LocalDateTime getLetrejott(){
+        return this.letrejott;
+    }
+    public LocalDateTime getSzerkeztve(){
+        return this.szerkeztve;
+    }
+    public void like(){
+            likeok++;
+    }
+
+
+
+}
